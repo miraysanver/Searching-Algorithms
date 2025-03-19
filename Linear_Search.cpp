34c43cc5fe4c9d491a529c31b58450f2c3dd3750
Miray Sanver <miraysanver04@gmail.com>
2025-03-19 15:10:27 +0300

    Linear_Search.cpp

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -1,20 +1,24 @@
-﻿// Linear_Search.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
-
-#include <iostream>
-
+﻿#include<iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int A[5] = { 67,90,34,62,45 };
+	bool bulundu = false;
+	int aranan;
+	cout << " aradiginiz degeri giriniz " << endl;
+	cin >> aranan;
+	for (int i = 0; i < 5; i++)
+	{
+		if (A[i] == aranan)
+		{
+			cout << " aranan deger " << i + 1 << ". siradadair " << endl;
+			bulundu = true;
+			break;
+		}
+	}
+	if (!bulundu)
+	{
+		cout << " aradiginiz deger bulunamadi " << endl;
+	}
+	return 0;
 }
-
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
-
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
